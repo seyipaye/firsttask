e72756cc0db521444ae3d09a216ad8dd46ef577e
Seyi <seyipaye@gmail.com>
2022-11-01 23:17:29 +0100

    fix7

--- .vscode/.history/Users/mac/Tech/Backend Python/HNG 9/firsttask/firsttask/settings_20221101231712.py
+++ .vscode/.history/Users/mac/Tech/Backend Python/HNG 9/firsttask/firsttask/settings_20221101231712.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from sys import path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_ROOT = Path.cwd().joinpath(BASE_DIR).joinpath('static')
+STATIC_ROOT = path.cwd().joinpath(BASE_DIR).joinpath('static')
 
 
 
